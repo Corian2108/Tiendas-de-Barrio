1c6c7836f3db4f2ad087d932d3086966b100bf47
Corian2108 <jefferson.ruiz2108@gmail.com>
2020-09-01 16:42:38 -0500

    Se agrega cambios nuevos al repositorio

---
+++ ProyectoTienda/ProyectoTienda/Data/MainContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProyectoTienda.Models;
+
+namespace ProyectoTienda.Data
+{
+    public class MainContext : DbContext
+    {
+        public MainContext (DbContextOptions<MainContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CompraCliente> CompraCliente { get; set; }
+
+        public DbSet<DueñoTienda> DueñoTienda { get; set; }
+    }
+}
---
+++ ProyectoTienda/ProyectoTienda/Models/Tienda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoTienda.Models
+{
+    public class Tienda
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Detalle { get; set; }
+        public List<Direccion> Direccion { get; set; } = new List<Direccion>();
+    }
+}
